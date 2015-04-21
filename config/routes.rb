98dc06e2fdd1d6e03069ac4bdc8e3d279ab146f6
Volodymyr Byno <volodymyr.byno@gmail.com>
2015-04-21 09:45:41 +0300

    add content to contact page; remove :about_us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
-  get :about_us,  to: 'static_pages#about_us'
   get :contacts,  to: 'static_pages#contacts'
   get :timetable, to: 'static_pages#timetable'
   get :articles,  to: 'static_pages#articles'
