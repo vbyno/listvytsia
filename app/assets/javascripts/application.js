81f4e87cca35d9b39c3340e23f34ada6f7d6b7e9
Volodymyr Byno <volodymyr.byno@gmail.com>
2015-05-24 00:06:48 +0300

    add google map to seminar page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,32 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore
+//= require gmaps/google
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  var lat = 49.798381,
+      lng = 24.035034;
+  handler = Gmaps.build('Google');
+  handler.buildMap({
+    provider: {
+      center: new google.maps.LatLng(lat, lng),
+      zoom: 15
+    },
+    internal: {
+      id: 'map'
+    }
+  },
+  function() {
+    markers = handler.addMarkers([
+      {
+        lat: lat,
+        lng: lng,
+        infowindow: "3 поверх"
+      }
+    ]);
+    handler.fitMapToBounds();
+  });
+});
