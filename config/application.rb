460f1903fe817b46b7fc07c6bc4bc10207bc8558
Volodymyr Byno <volodymyr.byno@gmail.com>
2015-08-22 12:26:21 +0300

    Use MongoDB. Mongoid instead of ActiveRecord;

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -35,6 +39,6 @@ module Listvytsia
     config.i18n.enforce_available_locales = false
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
