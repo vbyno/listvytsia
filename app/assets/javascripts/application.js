7969070dcf1436fad4e8dd5e6e6fd11b21caa4ff
Volodymyr Byno <volodymyr.byno@gmail.com>
2015-08-22 21:51:23 +0300

    CKeditor to edit HTML

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require underscore
 //= require gmaps/google
+//= require ckeditor/init
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root to: 'static_pages#index'
+
   get :contacts,  to: 'static_pages#contacts'
   get :timetable, to: 'static_pages#timetable'
   get :seminar,   to: 'static_pages#seminar'
+
   resources :contact_forms, only: :create
   resources :articles
+
+  mount Ckeditor::Engine => '/ckeditor'
 end
