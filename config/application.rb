77f3bad467eb0806615388a4950a9730054b3b31
Volodymyr Byno <volodymyr.byno@gmail.com>
2015-08-29 00:28:51 +0300

    Require mongoid in application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
+require 'rails/mongoid'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
