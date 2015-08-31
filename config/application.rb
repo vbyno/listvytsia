f71fc6fb7dc593e7eb0fdcc9eaf0431974451d74
Volodymyr Byno <volodymyr.byno@gmail.com>
2015-09-01 01:24:31 +0300

    Add gzip compression; Minify hero image

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,8 @@ module Listvytsia
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
+
+    # gzip
+    config.middleware.use Rack::Deflater
   end
 end
