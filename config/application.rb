c966c9056feaf50750c99c236d8a17c2de099e18
Volodymyr Byno <volodymyr.byno@gmail.com>
2016-04-14 07:46:20 +0300

    Disable gzip compression

--- config/application.rb
+++ config/application.rb
@@ -43,7 +43,7 @@ module Listvytsia
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
 
-    # gzip compression
-    config.middleware.insert_before(Rack::Sendfile, Rack::Deflater)
+    # gzip compression is not needed because it was implemented on Nginx layer
+    # config.middleware.insert_before(Rack::Sendfile, Rack::Deflater)
   end
 end
