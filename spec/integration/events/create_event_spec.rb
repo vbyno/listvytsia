f17db93ead3325c4201512e72755cadef4c42628
Volodymyr Byno <volodymyr.byno@gmail.com>
2016-06-06 21:24:20 +0300

    Tests for create event

--- spec/integration/events/create_event_spec.rb
+++ spec/integration/events/create_event_spec.rb
@@ -5,6 +5,7 @@ describe 'create event', js: true do
 
   before do
     allow_any_instance_of(AppComponent::EventPolicy).to receive(:create?).and_return true
+    allow_any_instance_of(AppComponent::EventPolicy).to receive(:update?).and_return true
   end
 
   it 'creates' do
@@ -12,9 +13,23 @@ describe 'create event', js: true do
 
     visit '/events/new'
 
-    click_link 'Seo Content'
+    fill_in 'Заголовок', with: 'Подія 1'
+    fill_in 'Посилання', with: 'podiya_1'
+    fill_in 'Ввідний текст', with: 'Чудова стаття, написана хорошим автором'
 
-    fill_in 'SEO-заголовок', with: 'Seo-заголовок цієї чудової сторінки'
-    fill_in 'SEO-опис', with: 'Seo-опис сторінки'
+    click_button 'Зберегти'
+    expect(page).to have_error 'Контент не може бути пустим'
+
+    fill_in_ckeditor 'event_content', with: 'Наповнення статті'
+
+    click_button 'Зберегти'
+    expect(page).to have_notice 'Подія успішно створена'
+
+    click_link 'Подія 1'
+    click_link 'Редагувати подію'
+    click_link 'Фотографії'
+    click_link 'Додати фотографії'
+    attach_file('ok', Rails.root.join('spec', 'support', 'fixtures', 'image.jpeg'))
+    page.attach_file "images", ['path to file1.jpg', 'pathto file2.jpg' ,'pathtofile3.JPG']
   end
 end
