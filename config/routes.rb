6b6b1e4c83a6a96b57ec0ac6042aa8e90aed4f4c
Volodymyr Byno <volodymyr.byno@gmail.com>
2016-06-08 08:21:13 +0300

    www Redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  constraints subdomain: 'www' do
+    get ':any', to: redirect(subdomain: nil, path: '/%{any}'), any: /.*/
+  end
+
   mount AppComponent::Engine, at: '/'
 end
