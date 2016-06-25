07110900b82d2ee4105e794442b1b6e67c2fb884
Volodymyr Byno <volodymyr.byno@gmail.com>
2016-06-25 23:43:56 +0300

    Core::ApplicationController CSRF protection

--- components/core/app/controllers/core/application_controller.rb
+++ components/core/app/controllers/core/application_controller.rb
@@ -3,6 +3,7 @@ module Core
     include ::Pundit
 
     protect_from_forgery with: :exception
+    after_filter :set_csrf_cookie_for_ng
 
     rescue_from ::Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -10,7 +11,17 @@ module Core
 
     layout 'application'
 
+    protected
+
+    def verified_request?
+      super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+    end
+
     private
+
+    def set_csrf_cookie_for_ng
+      cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+    end
 
     def pundit_user
       current_user || MissedUser.instance
