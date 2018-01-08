c2fa2d972469c117b59b4698034387ed1f6384a3
Volodymyr Byno <volodymyr.byno@gmail.com>
2018-01-08 16:38:54 +0200

    Change main menu to display all regions

--- components/micro_groups/spec/lib/micro_groups_spec.rb
+++ components/micro_groups/spec/lib/micro_groups_spec.rb
@@ -15,4 +15,41 @@ describe MicroGroups do
       end
     end
   end
+
+  describe '.links_info' do
+    subject { described_class.links_info }
+
+    context 'no regions exist' do
+      it 'returns empty array' do
+        is_expected.to be_empty
+      end
+    end
+
+    context 'region exists' do
+      let!(:region) { create :region }
+
+      it 'returns region name and region permalink' do
+        subject.first.tap do |region_info|
+          expect(region_info.permalink).to eq region.permalink
+          expect(region_info.name).to eq region.name
+        end
+      end
+    end
+
+    context 'several regions exist' do
+      let!(:region_1) { create :region, priority: 1 }
+      let!(:region_2) { create :region, priority: 2 }
+
+      it 'sorts regions by priority' do
+        expect(subject.first.permalink).to eq region_2.permalink
+        expect(subject.last.permalink).to eq region_1.permalink
+      end
+    end
+
+    context 'unpublished' do
+      let!(:region) { create :region, published: false }
+
+      it { is_expected.to be_empty }
+    end
+  end
 end
